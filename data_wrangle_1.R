f6179a81934c0e0a74e307c0d94ade9a52bd9ba6
Zane Rodnick-Smith <zrodnick@gmail.com>
2016-11-04 15:46:45 -0500

    Lower case table before spelling errors are corrected.

--- data_wrangle_1.R
+++ data_wrangle_1.R
@@ -1,4 +1,5 @@
 refine_raw <- read.csv("refine.csv")
 refine_tbl <- tbl_df(refine_raw)
 
-refine_tbl$company <- tolower(refine_tbl$company)
+refine_tbl$company <- tolower(refine_tbl$company) %>%
+gsub(c("fillips", "phillips", "phillps", "phlips", "phllips"), "philips", refine_tbl$company)

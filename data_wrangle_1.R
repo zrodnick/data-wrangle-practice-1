ef8aa14190656eafef8044c0d2c4edd72dc51b7f
Zane Rodnick-Smith <zrodnick@gmail.com>
2016-11-04 17:58:06 -0500

    Cleaning up names, making new columns

--- data_wrangle_1.R
+++ data_wrangle_1.R
@@ -1,5 +1,18 @@
+library(dplyr)
+library(tidyr)
+
 refine_raw <- read.csv("refine.csv")
 refine_tbl <- tbl_df(refine_raw)
 
-refine_tbl$company <- tolower(refine_tbl$company) %>%
-gsub(c("fillips", "phillips", "phillps", "phlips", "phllips"), "philips", refine_tbl$company)
+refine_tbl$company <- tolower(refine_tbl$company)
+refine_tbl$company <- gsub("phlips|phillps|phlips|phllips", "philips", refine_tbl$company)
+refine_tbl$company <- gsub("ak zo|akz0", "akzo", refine_tbl$company)
+refine_tbl$company <- gsub("unilver", "unilever", refine_tbl$company)
+
+refine_tbl2 <- separate(refine_tbl, Product.code...number, c("Product.code", "number"))
+
+refine_tbl3 <- mutate(refine_tbl2, Product.category =
+                        ifelse(grepl("p", Product.code), "Smartphone",
+                               ifelse(grepl("v", Product.code), "TV",
+                               ifelse(grepl("x", Product.code), "Laptop",
+                               ifelse(grepl("q", Product.code), "Tablet", "NA")))))
